836cd400908f275b2445191ee9fefb7be9313913
Shaun-F <shaundbfell@gmail.com>
2022-11-11 23:26:54 +0100

    added numerical interpolator

--- GWGen/WFGenerator/ModifiedKludge.py
+++ GWGen/WFGenerator/ModifiedKludge.py
@@ -1,7 +1,17 @@
 from .Kludge import *
+from GWGen.numerical_flux import *
+from GWGen.DressedFluxes import *
 
 
 
+class FluxFunction(name="analytic"):
+    def __init__(self):
+        if name=="analytic":
+            self.EFlux = Analytic5PNEFlux
+            self.LFlux = Analytic5PNLFlux
+        elif name=="numerical":
+            self.EFlux = GenerateNumericalEInterpolation()
+            self.LFlux = GenerateNumericalEInterpolation()
 
 class ModifiedKludgeWaveform():
     def __init__(self):
